5748fa18962b7711186755312525d4ed24504901
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2023-12-28 14:26:58 +0100

    Photo page pain

--- Yearly.MauiClient/Services/SharpApiFacades/SharpAPIClient.cs
+++ Yearly.MauiClient/Services/SharpApiFacades/SharpAPIClient.cs
@@ -1,19 +1,20 @@
 ﻿namespace Yearly.MauiClient.Services.SharpApiFacades;
 
-
 public class SharpAPIClient
 {
     private readonly HttpClient _httpClient;
     public HttpClient HttpClient => _httpClient;
 
-    private const string k_BaseAddress = "https://localhost:7217";
-    //private const string k_BaseAddress = "http://192.168.1.113:1337";
-    //private const string k_BaseAddress = "http://10.0.2.2:5281";
+    private const string k_BaseAddressUWP = "https://localhost:7217";
+    private const string k_BaseAddressAndroid = "http://10.0.2.2:5281";
+    private const string k_BaseAddressExternalDevice = "http://192.168.1.113:1337";
 
     public SharpAPIClient()
     {
         _httpClient = new();
-        _httpClient.BaseAddress = new Uri(k_BaseAddress);
+        _httpClient.BaseAddress = new Uri(
+            DeviceInfo.Platform == DevicePlatform.Android ? k_BaseAddressAndroid : k_BaseAddressUWP); //Todo: set link to cloud hosted
+        //HttpClient.BaseAddress = new Uri(k_BaseAddressExternalDevice);
     }
 
     /// <summary>
