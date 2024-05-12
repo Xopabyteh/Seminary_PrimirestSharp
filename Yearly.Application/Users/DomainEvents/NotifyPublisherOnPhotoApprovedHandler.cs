90a8dbe08af71f8d6cf307fe3851820cced2c4d9
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2024-05-12 16:26:32 +0200

    ux tweak

--- Yearly.Application/Users/DomainEvents/NotifyPublisherOnPhotoApprovedHandler.cs
+++ Yearly.Application/Users/DomainEvents/NotifyPublisherOnPhotoApprovedHandler.cs
@@ -2,6 +2,7 @@
 using Yearly.Application.Common.Interfaces;
 using Yearly.Contracts.Notifications;
 using Yearly.Domain.Models.UserAgg.DomainEvents;
+using Yearly.Domain.Models.UserAgg.ValueObjects;
 using Yearly.Domain.Repositories;
 
 namespace Yearly.Application.Users.DomainEvents;
@@ -9,12 +10,14 @@ namespace Yearly.Application.Users.DomainEvents;
 public class NotifyPublisherOnPhotoApprovedHandler : INotificationHandler<UserApprovedPhotoDomainEvent>
 {
     private readonly IUserNotificationService _notificationService;
+    private readonly IUserRepository _userRepository;
     private readonly IPhotoRepository _photoRepository;
 
-    public NotifyPublisherOnPhotoApprovedHandler(IUserNotificationService notificationService, IPhotoRepository photoRepository)
+    public NotifyPublisherOnPhotoApprovedHandler(IUserNotificationService notificationService, IPhotoRepository photoRepository, IUserRepository userRepository)
     {
         _notificationService = notificationService;
         _photoRepository = photoRepository;
+        _userRepository = userRepository;
     }
 
     public async Task Handle(UserApprovedPhotoDomainEvent request, CancellationToken cancellationToken)
@@ -23,6 +26,13 @@ public class NotifyPublisherOnPhotoApprovedHandler : INotificationHandler<UserAp
 
         if (photo is null)
             return; // The photo could've been deleted before the event was handled
+
+        var publisher = await _userRepository.GetByIdAsync(photo.PublisherId);
+        if(publisher is null)
+            return; // The publisher could've been deleted before the event was handled
+
+        if (publisher.Roles.Contains(UserRole.PhotoApprover))
+            return; // Don't notify photo approvers themselves
 
         await _notificationService.SendPushNotificationAsync(
             "Díky",
